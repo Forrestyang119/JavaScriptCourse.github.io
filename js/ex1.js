91240ab7ceaf8a6f38e83485794a2588f3d7f105
Forrest <sy358@scarletmail.rutgers.edu>
2017-03-03 08:29:06 -0500

    first commit

--- js/ex1.js
+++ js/ex1.js
@@ -1,17 +1,17 @@
+/* call function when the html document is loaded */
 $(document).ready(function() {
-
+	/* perform action when button is clicked */
     $('button').click(function() {
-        $('.vanish').fadeOut('slow');
-        /* more actions here */
-        // $('.vanish').fadeTo('fast', 0.5);
+       	/* get by id (jQuery) */
+		/* hide the first block, element accessed by id */
+		$('#first').hide();
 
+		/* get by class name (jQuery) */
+		/* change the color of blocks with class name as "appear" */
+        $('.appear').css('background-color','red');
+
+        /* get by tag name (pure javascript) */
+		/* change the height of elements with tag name as "div" */
+		$('div').height('400px');
     });
-
-    $('button').click(function() {
-        $('#third').fadeOut('slow');
-        /* more actions here */
-        // $('.vanish').fadeTo('fast', 0.5);
-
-    });
-
 });
